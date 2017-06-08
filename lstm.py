615ccd43337cafbaf093858135e92f6d28826dcc
liang@fedora <liang.ma@studenti.polito.it>
2017-06-08 10:06:19 +0200

    increased dropout rate to avoid underfit

--- lstm.py
+++ lstm.py
@@ -8,6 +8,7 @@ with open('dataset_63642samples_with_noise.csv', 'rb') as csvfile:
     for row in creader:
         dataList.append(row)
 dArray = np.asarray(dataList, dtype=float)
+N = dArray.shape[1]
 dDev = np.std(dArray, 1)
 dMean = np.mean(dArray, 1)
 for i in range(dArray.shape[1]):
@@ -15,10 +16,11 @@ for i in range(dArray.shape[1]):
 
 valid_size = 1000
 valid_set = dArray[:,:valid_size]
-train_set = dArray[:,valid_size:] 
+test_set = dArray[:, valid_size:valid_size*5]
+train_set = dArray[:,valid_size*5:] 
 
 lstm_size = 64
-batch_size=50
+batch_size = 50
 num_unrollings=1
 input_size = 4
 output_size = 2
@@ -49,11 +51,13 @@ class GenerateBatchData(object):
         return batches, targets
 train_gen = GenerateBatchData(train_set, batch_size, num_unrollings)
 valid_gen = GenerateBatchData(valid_set, 1, 1)
+test_gen = GenerateBatchData(test_set, 1, 1)
 batch, label = train_gen.next()
 #print("batch:", batch)
 #print("label:", label)            
-extent_size = 32
-hidden_size = 32
+extent_size = 50
+hidden_size = 25
+dropRate = 0.9
 graph=tf.Graph()
 with graph.as_default():
 
@@ -68,8 +72,8 @@ with graph.as_default():
         """Create a LSTM cell. See e.g.: http://arxiv.org/pdf/1402.1128v1.pdf
         Note that in this formulation, we omit the various connections between the
         previous state and the gates."""
-        ie = tf.nn.relu(tf.nn.dropout(tf.matmul(i, w0)+b0,0.5))
-        _matmul=tf.nn.dropout(tf.matmul(ie, lx),0.5) + tf.matmul(o, lm) + lb
+        ie = tf.nn.relu(tf.nn.dropout(tf.matmul(i, w0)+b0,dropRate))
+        _matmul=tf.nn.dropout(tf.matmul(ie, lx),dropRate) + tf.matmul(o, lm) + lb
         input_gate, forget_gate, update, output_gate = tf.split(_matmul, 4, 1)
         state = tf.sigmoid(forget_gate) * state + tf.sigmoid(input_gate) * tf.tanh(update)
         return tf.sigmoid(output_gate) * tf.tanh(state), state
@@ -91,8 +95,8 @@ with graph.as_default():
     b2 = tf.Variable(tf.zeros([output_size]), dtype=tf.float32)
 
     def train_feed_model(output):
-        hidden_layer = tf.nn.relu(tf.nn.dropout(tf.matmul(output, w1) +
-                    b1,0.5))
+        hidden_layer = tf.nn.dropout(tf.nn.relu(tf.matmul(output, w1) +
+                    b1),dropRate)
         predictions = tf.matmul(hidden_layer,w2) + b2
         return predictions
     def feed_model(output):
@@ -120,13 +124,18 @@ with graph.as_default():
     with tf.control_dependencies([init_state.assign(state),
                 init_output.assign(output)]):
         outCat = tf.concat(outputs,0)
-        predictions = feed_model(outCat)
+        predictions = train_feed_model(outCat)
+
+        # absolute error
         loss = tf.losses.mean_squared_error(tf.concat(train_targets,0),
                 predictions)
+        # relative error
+        reloss = tf.reduce_mean(
+                    tf.square(predictions/tf.concat(train_targets,0)- 1.0))
     
     global_step = tf.Variable(0)
     learning_rate = tf.train.exponential_decay(
-        0.5, global_step, 300, 0.9)
+        1.0, global_step,500, 0.8)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
     gradients, v = zip(*optimizer.compute_gradients(loss))
     gradients, _ = tf.clip_by_global_norm(gradients, 1.25)
@@ -148,7 +157,7 @@ with graph.as_default():
                                 init_valid_state.assign(valid_state)]):
         sample_prediction = feed_model(valid_output)
 
-num_steps = 1201
+num_steps = (N - valid_size)//batch_size
 summary_frequency = 100
 
 with tf.Session(graph=graph) as session:
@@ -175,16 +184,23 @@ with tf.Session(graph=graph) as session:
             mean_loss = 0
     #        print("predictions:", p)
     #        print("targets:", labels)
+            reset_state.run() 
+            valid_loss = 0;
+            test_size = valid_size 
+            for _ in range(test_size):
+                vb,vl = valid_gen.next()
+                predict = sample_prediction.eval({valid_input: vb[0]})
+                valid_loss = valid_loss + ((predict - vl[0])**2).mean()
+            print('Validation mean loss: %.2f' % float(valid_loss / test_size))
     reset_state.run() 
     valid_loss = 0;
-    test_size = valid_size
+    test_size = valid_size * 4 
+    display = test_size * 0.8
     for i in range(test_size):
-        vb,vl = valid_gen.next()
+        vb,vl = test_gen.next()
         predict = sample_prediction.eval({valid_input: vb[0]})
-        valid_loss = valid_loss + ((predict-vl[0])**2).mean()
-        if i> (test_size - 10):
-            print(i,'='*80)
-            print("frequencies:", vb[0])
-            print("positions:", vl[0])
-            print("predictions:",predict)
-    print('Validation mean loss: %.2f' % float(valid_loss / test_size))
+        valid_loss = valid_loss + ((predict - vl[0])**2).mean()
+        if i > display and i < display + 10:
+            print("positions:", vl[0]*dDev[4:]+dMean[4:])
+            print("predictions:", predict*dDev[4:]+dMean[4:])
+    print('Testing mean loss: %.2f' % float(valid_loss / test_size))
